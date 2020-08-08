d5772f33fe8c9a12245fdd12afa8c14abb966fe6
abpun <56480651+abhishek-png@users.noreply.github.com>
2020-08-08 18:44:24 +0545

    update

--- chat/index.js
+++ chat/index.js
@@ -3,8 +3,6 @@ const app = express();
 const StreamChat = require('stream-chat').StreamChat;
 const serverClient = new StreamChat('gkd73zy53ast', '7r8ehth3tzk6nupjkryr8gktsbtnt94v7be4h9scp8ncvvv2sv6zs92ag25vebs3');
 
-
-
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
